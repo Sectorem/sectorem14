d5f78a58ed9b16ba6395d9d6b77d84ed95749c72
Andrew <Andrew@Andrew-Komputer>
2014-08-13 17:57:46 +0200

    test commit 2

---
+++ EmptyPHP.php
@@ -0,0 +1,8 @@
+<?php
+<p> tu nic nie ma <\p>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+>
